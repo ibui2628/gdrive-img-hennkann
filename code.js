8c484b098f177c73463fa2378852a999ff3a3727
ibui2628 (I-Koubou) <92432744+ibui2628@users.noreply.github.com>
2022-07-23 09:24:13 +0900

    Add files via upload

---
+++ code.js
@@ -0,0 +1,19 @@
+document.getElementById("kennse").style.display ="none";
+function js() {
+const gdive1 = document.getElementById("gdive")
+const gdive2 = gdive1.value
+const ken = gdive2.substr(8 ,16);
+if (ken == "drive.google.com") {
+const code = gdive2.substr(32 ,33);
+const imgurl = "https://drive.google.com/uc?id="+code;
+document.getElementById("kennse").style.display ="block";
+document.getElementById( "end" ).value = imgurl ;
+} else {
+alert("正しいURLを入力してください")
+}
+}
+function copy(){
+var copyTarget = document.getElementById("end");
+copyTarget.select();
+document.execCommand("Copy");
+}
